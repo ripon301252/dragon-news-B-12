7f3d87ab06b766db03d8dc357be0491a63fabaee
Mahfuzur Rahman <mahfuzurrahman0012@gmail.com>
2025-10-18 22:05:45 +0600

    Added new

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            Home
+            <h2 className='font-bold'>Home</h2>
         </div>
     );
 };
--- src/layouts/HomeLayOut.jsx
+++ src/layouts/HomeLayOut.jsx
@@ -20,13 +20,13 @@ const HomeLayOut = () => {
                 <Navbar></Navbar>
             </nav>
             <main className='w-11/12 mx-auto my-3  {*:border} grid md:grid-cols-12 grid-cols-1 gap-5'>
-                <aside className='md:col-span-3 col-span-6'>
+                <aside className='md:col-span-3 col-span-6 sticky top-0 h-fit '>
                     <LeftAside></LeftAside>
                 </aside>
                 <section className='col-span-6'>
                     <Outlet></Outlet>
                 </section>
-                <aside className='md:col-span-3 col-span-6'>
+                <aside className='md:col-span-3 col-span-6 sticky top-0 h-fit'>
                     <RightAside></RightAside>
                 </aside>
             </main>
